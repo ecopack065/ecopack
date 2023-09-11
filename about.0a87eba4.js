9cab5a1dce34d6addaeb310d979952915815b9ad
ecopack065 <144681223+ecopack065@users.noreply.github.com>
2023-09-11 14:16:23 +0300

    Add files

---
+++ about.0a87eba4.js
@@ -0,0 +1,3 @@
+!function(){let e={burgerIcon:document.querySelector(".burger-icon"),closeIcon:document.querySelector(".icon-x"),modalmenu:document.querySelector(".modal-menu")};e.burgerIcon.addEventListener("click",()=>{e.modalmenu.classList.remove("is-hidden")}),e.closeIcon.addEventListener("click",()=>{e.modalmenu.classList.add("is-hidden")})}();//# sourceMappingURL=about.0a87eba4.js.map
+
+//# sourceMappingURL=about.0a87eba4.js.map
---
+++ about.ee47a133.js
@@ -0,0 +1,3 @@
+const e={burgerIcon:document.querySelector(".burger-icon"),closeIcon:document.querySelector(".icon-x"),modalmenu:document.querySelector(".modal-menu")};e.burgerIcon.addEventListener("click",()=>{e.modalmenu.classList.remove("is-hidden")}),e.closeIcon.addEventListener("click",()=>{e.modalmenu.classList.add("is-hidden")});//# sourceMappingURL=about.ee47a133.js.map
+
+//# sourceMappingURL=about.ee47a133.js.map
